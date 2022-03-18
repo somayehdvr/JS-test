67b74455e3dc1456217810272e5d2704ca7cb7bb
somayehdvr <61237342+somayehdvr@users.noreply.github.com>
2022-03-18 19:34:03 +0100

    Update async.js

--- async.js
+++ async.js
@@ -2,13 +2,12 @@
 // https://reqres.in/api/users
 // Display the first_name of the first three users in the DOM
 // const body = document.querySelector("body")
-const p = []
 fetch("https://reqres.in/api/users")
     .then(response => response.json())
     .then(results => {
         for (let i = 0; i < 3; i++){
-            p[i] = document.createElement("p")
-            p[i].innerHTML += results.data[i].first_name
-            body.appendChild(p[i])
+            const p = document.createElement("p")
+            p.innerHTML += results.data[i].first_name
+            body.appendChild(p)
         }
 })
